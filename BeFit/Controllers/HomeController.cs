59fb13023592547f35693149840f4a27d6503251
Krzysztof Żebrowski <maxnoe7@gmail.com>
2026-04-30 22:29:01 +0200

    Dodanie importu sesji, walidacji. Ograniczenie widoku niepotrzebnych przycisków
    Dodano import i czyszczenie sesji w HomeController. Zaktualizowano widoki i menu na polskie, dodano sekcję zarządzania danymi testowymi oraz przykładowy plik sessions.json.

--- BeFit/Controllers/HomeController.cs
+++ BeFit/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using BeFit.Data;
 using BeFit.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,19 +10,28 @@ namespace BeFit.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        // Szybkie przełączanie między kontami użytkowników
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        // Import przykładowych danych 
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+
         public HomeController(
             ILogger<HomeController> logger,
             SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager)
+            UserManager<IdentityUser> userManager,
+            ApplicationDbContext context,
+            IWebHostEnvironment environment)
         {
             _logger = logger;
             _signInManager = signInManager;
             _userManager = userManager;
+            _context = context;
+            _environment = environment;
         }
 
-        // Szybkie przełączanie między kontami użytkowników
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
 
         [HttpPost]
         public async Task<IActionResult> SwitchUser(string email)
@@ -32,6 +42,47 @@ namespace BeFit.Controllers
                 await _signInManager.SignOutAsync();
                 await _signInManager.SignInAsync(user, isPersistent: false);
             }
+            return RedirectToAction("Index");
+        }
+
+        // import danych z pliku JSON
+        [HttpPost]
+        public async Task<IActionResult> ImportData(IFormFile file)
+        {
+            string filePath = Path.Combine(_environment.ContentRootPath, "sessions.json");
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var json = await System.IO.File.ReadAllTextAsync(filePath);
+                var options = new System.Text.Json.JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var sessions = System.Text.Json.JsonSerializer.Deserialize<List<Session>>(json, options);
+                if (sessions != null )
+                {
+
+                    var currentUserId = _userManager.GetUserId(User);
+
+                    foreach (var session in sessions)
+                    {
+                        session.Id = 0; 
+                        session.UserId = currentUserId;
+                    }
+
+                    _context.Session.AddRange(sessions);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Czyszczenie rekordów sesji treningowych
+        [HttpPost]
+        public async Task<IActionResult> ClearSessions()
+        {
+            _context.Session.RemoveRange(_context.Session);
+            await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
--- BeFit/Models/ExerciseType.cs
+++ BeFit/Models/ExerciseType.cs
@@ -6,7 +6,7 @@ namespace BeFit.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "Proszę wprowadzić nazwę ćwiczenia")]
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Nazwa musi mieć od 5 do 50 znaków.")]
         [Display(Name = "Nazwa ćwiczenia")]
         public string Name { get; set; }
